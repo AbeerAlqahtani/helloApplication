b81be5b3f4610b518a92b6e6b5cbf55f44746cbd
AbeerAlqahtani <abeer_albeshry@hotmail.com>
2020-09-18 10:08:24 +0300

    update

--- app/src/main/java/com/example/myapplication/HelloActivity.java
+++ app/src/main/java/com/example/myapplication/HelloActivity.java
@@ -8,8 +8,8 @@ import android.widget.TextView;
 
 public class HelloActivity extends AppCompatActivity {
 
-//abeer
-    
+    // abeer
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
